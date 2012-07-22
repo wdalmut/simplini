6022448de3e4670b5cf7b1233bc73adee3569142
Walter Dal Mut <walter.dalmut@gmail.com>
2012-07-22 10:52:05 +0200

    Added tests

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -130,4 +130,72 @@ class ConfigTest extends PHPUnit_Framework_TestCase
         $this->assertEquals("localhost", $this->object->mysql()->host);
         $this->assertEquals("KO", $this->object->third()->value);
     }
+    
+    /**
+     * @expectedException RuntimeException
+     */
+    public function testMagicSet()
+    {
+        $this->object->hello = "ciao";
+    }
+    
+    public function testMagicGet()
+    {
+        $this->assertFalse($this->object->nonExistingKey);
+        
+        $this->assertFalse(array_key_exists("anotherFalse", $this->object));
+    }
+    
+    public function testToArray()
+    {
+        $array = array("ca" => "cu", "a"=>array("b" => 'c'));
+        $o = new Config($array);
+        
+        $t = $o->toArray();
+        
+        $this->assertThat($t, $this->equalTo($array));
+    }
+    
+    /**
+     * @expectedException RuntimeException
+     */
+    public function testOffsetSet()
+    {
+        $this->object->load(__DIR__ . '/configs/array.ini');
+        
+        $this->object->w[] = "hello";
+    }
+    
+    public function testOffsetExists()
+    {
+        $this->object->load(__DIR__ . '/configs/simple.ini');
+        $this->assertTrue(isset($this->object["hello"]));
+    }
+    
+    public function testOffsetGet()
+    {
+        $this->object->load(__DIR__ . '/configs/simple.ini');
+        
+        $this->assertFalse($this->object["helloWorld"]);
+    }
+    
+    /**
+     * @expectedException RuntimeException
+     */
+    public function testOffsetUnset()
+    {
+        $this->object->load(__DIR__ . '/configs/simple.ini');
+        
+        unset($this->object["hello"]);
+    }
+    
+    /**
+     * @expectedException RuntimeException
+     */
+    public function testUnset()
+    {
+        $this->object->load(__DIR__ . '/configs/simple.ini');
+        
+        unset($this->object["hello"]);
+    }
 }
